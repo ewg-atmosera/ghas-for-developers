0db623e3c6f102c3c8c32b1fb5614fdd38b36c29
Andrew C. Scoppa <andrew-scoppa@github.com>
2024-10-11 10:59:48 -0400

    Sync local repository with remote repository

--- .github/wf-scripts/sample.sh
+++ .github/wf-scripts/sample.sh
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 if [ "$message" ]; then
     echo $message
     echo $repo_owner
@@ -6,14 +5,3 @@ if [ "$message" ]; then
 else
     echo "A welcome message was not provided."
 fi
-=======
-set -x
-if [ "$message" ]; then
-    echo $message
-    echo $repo_owner
-    gh issue create --title "$message" --body $repo_owner" requests comments on the message..."  
-else
-    echo "A welcome message was not provided."
-fi
-
->>>>>>> 4677756daa20bb6bfe11c663ae9ed08b7946bd2e
